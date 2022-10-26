231c0b64ebef4af57bef236cac46e660f8d4ac94
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-26 10:33:21 +0000

    Uploading files

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""base_model module contains the BaseModel class implementation"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """"BaseModel : a base class for common attributes and methods for futures
+        classes"""
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Readabe representation of an instance"""
+        return "[Basemodel] ({}) {}".format(self.id, self.__dict__)
+
+    def to_dict(self):
+        instance_dict = self.__dict__
+
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat('T')
+        instance_dict['updated_at'] = self.updated_at.isoformat('T')
+        return instance_dict
+
+    def save(self):
+        self.updated_at = datetime.now()
---
+++ models/clear.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+clear - clears the python shell
+
+Prototype: def clear()
+
+Return: Nothing
+"""
+def clear():
+    import os
+    os.system('clear')
