fd2d00f1ab2b55ac8f7ce27728d59ee9a40bae83
Arinze Nwutobo <tim.arinze@outlook.com>
2022-10-30 17:33:54 +0100

    task

--- models/__init__.py
+++ models/__init__.py
@@ -6,5 +6,7 @@ creating an instance of FileStorage for this application
 
 
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,26 +2,27 @@
 
 """ BaseModel that defines all common attributes/methods for other classes """
 
-import uuid
+from uuid import uuid4
 from datetime import datetime
+import models
 
 class BaseModel:
     """ Base Model """
     def __init__(self, *args, **kwargs):
         """initialization of the class"""
 
-        if kwargs is not None:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key == '__class__':
                     continue
                 setattr(self, key, value)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """__str__"""
