2e1832becdc0a72115b4857287d001b8bce343ca
Arinze Nwutobo <tim.arinze@outlook.com>
2022-10-31 16:28:19 +0100

    task

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,6 @@
 
 from uuid import uuid4
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -15,18 +14,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialization of the class"""
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key == '__class__':
-                    continue
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """__str__"""
